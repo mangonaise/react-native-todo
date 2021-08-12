9c8d76b3518372af73c00a08d977058bc7f81a9c
= <liam2robertson@gmail.com>
2021-08-12 16:27:16 +0100

    Replace modalize with react-native-modal

--- components/ListSelectModal.tsx
+++ components/ListSelectModal.tsx
@@ -1,39 +1,38 @@
 import React from 'react';
-import { useEffect } from 'react';
 import { useContext } from 'react';
-import { useRef } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Modalize } from 'react-native-modalize';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+import Modal from 'react-native-modal';
 import AppContext from '../logic/appContext';
 import List from '../logic/list';
 
 interface Props {
   isOpen: boolean,
   onEditList: (list: List) => void,
-  onClose: () => void
+  hide: () => void
 }
 
-const ListSelectModal = ({ isOpen, onEditList, onClose }: Props) => {
+const ListSelectModal = ({ isOpen, onEditList, hide }: Props) => {
   const app = useContext(AppContext);
-  const modalRef = useRef<Modalize>(null);
 
   function handleSelectList(list: List) {
     app.setActiveList(list);
-    closeModal();
+    hide();
   }
 
-  function closeModal() {
-    modalRef.current?.close();
+  function handleEditList(list: List) {
+    onEditList(list);
+    hide();
   }
-
-  useEffect(() => {
-    if (isOpen) {
-      modalRef.current?.open();
-    }
-  }, [isOpen])
 
   return (
-    <Modalize onClose={onClose} ref={modalRef} adjustToContentHeight>
+    <Modal 
+      isVisible={isOpen} 
+      onDismiss={hide} 
+      onBackdropPress={hide}
+      useNativeDriverForBackdrop 
+      style={styles.modal}>
       <View style={styles.container}>
         {app.lists.map((list, index) => (
           <View key={index} style={styles.row}>
@@ -43,14 +42,23 @@ const ListSelectModal = ({ isOpen, onEditList, onClose }: Props) => {
               activeOpacity={0.6}>
               <Text>{list.name}</Text>
             </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.editListButton}
+              onPress={() => handleEditList(list)}>
+              <FontAwesomeIcon icon={faEllipsisH} color="#8a8a8a" />
+            </TouchableOpacity>
           </View>
         ))}
       </View>
-    </Modalize>
+    </Modal>
   )
 }
 
 const styles = StyleSheet.create({
+  modal: {
+    justifyContent: 'flex-end',
+    margin: 0
+  },
   container: {
     width: '100%',
     backgroundColor: '#d9d9d9'
@@ -65,6 +73,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     justifyContent: 'center',
     flexGrow: 1
+  },
+  editListButton: {
+    backgroundColor: 'white',
+    height: 50,
+    paddingHorizontal: 15,
+    justifyContent: 'center',
   }
 });
 
